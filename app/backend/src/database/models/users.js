bc9feedb2ff64bba58a9efee96b26d34899188f0
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-05-30 21:09:19 -0300

    feat(User): Criação da model e migration de users

---
+++ app/backend/src/database/models/users.js
@@ -0,0 +1,15 @@
+const modelUsers = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    role: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    underscored: true,
+    timestamps: false,
+  });
+
+  return User;
+}
+
+module.exports = modelUsers;
