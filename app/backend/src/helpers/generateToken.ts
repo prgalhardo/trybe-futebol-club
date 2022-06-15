5ad5cb69a40ffae42e4e647c3c9e1dec997b94a0
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-06-15 17:16:49 -0300

    feat(Token): Criação chave token

---
+++ app/backend/src/helpers/generateToken.ts
@@ -0,0 +1,16 @@
+import * as Jwt from 'jsonwebtoken';
+import * as fs from 'fs';
+
+class Token {
+  private static secret: string = fs.readFileSync('jwt.evaluation.key', 'utf-8');
+
+  static create(info: object) {
+    return Jwt.sign(info, this.secret, { expiresIn: '7d ' });
+  }
+
+  static decode(token: string) {
+    return Jwt.verify(token, this.secret);
+  }
+}
+
+export default Token;
