63ce4f3b326e757b273d9eecd1fedff1f5028f91
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-06-17 21:30:22 -0300

    feat(LoginMiddlewares): Implementação das validações da rota post de login

---
+++ app/backend/src/middlewares/loginMiddleware.ts
@@ -0,0 +1,29 @@
+import validator from 'validator';
+import { Request, Response, NextFunction } from 'express';
+
+const messageOne = 'Incorrect email or password';
+
+function validateEmail(email: string) {
+  if (!email) return ({ status: 400, message: 'All fields must be filled' });
+  if (validator.isEmpty(email)) return ({ status: 401, message: messageOne });
+  if (typeof email !== 'string') return ({ status: 401, message: messageOne });
+}
+
+function validatePassword(password: string) {
+  if (!password) return ({ status: 400, message: 'All fields must be filled' });
+  if (typeof password !== 'string') return ({ status: 401, message: messageOne });
+  if (password.length <= 6) return ({ status: 401, message: messageOne });
+}
+
+function validateLoginBody(req: Request, res: Response, next: NextFunction) {
+  const { email, password } = req.body;
+  let error = validateEmail(email);
+  if (error) return res.status(error.status).json({ message: error.message });
+
+  error = validatePassword(password);
+  if (error) return res.status(error.status).json({ message: error.message });
+
+  next();
+}
+
+export default validateLoginBody;
--- app/backend/src/routes/login.routes.ts
+++ app/backend/src/routes/login.routes.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express';
-import UserService from '../services/loginService';
+import validateLoginBody from '../middlewares/loginMiddleware';
+import LoginController from '../controllers/loginController';
 
 const loginRoutes = Router();
-const loginInfos = new UserService();
+const loginInfos = new LoginController();
 
-loginRoutes.post('/login', async (req, res, _next) => {
-  const { email, password } = req.body;
-  const result = await loginInfos.create(email, password);
-  return res.status(200).json(result);
-});
+loginRoutes.post('/login', validateLoginBody, loginInfos.create);
 
 export default loginRoutes;
