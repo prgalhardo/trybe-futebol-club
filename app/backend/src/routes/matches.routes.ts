5f10c576c3307cac5908767cdd27f0e0bce39b73
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-06-19 20:22:13 -0300

    feat(Correção): Implementação de autorização do token na requisição post da rota matches

--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import MatchesController from '../controllers/matchesController';
 import validateEqualTeams, { validateTeam } from '../middlewares/matchesMiddlewares';
+import TokenAuthorization from '../middlewares/tokenMiddleware';
 
 const matchesRoutes = Router();
 const matchesInfos = new MatchesController();
 
 matchesRoutes.get('/matches', matchesInfos.findAll);
-matchesRoutes.post('/matches', validateTeam, validateEqualTeams, matchesInfos.create);
+matchesRoutes.post(
+  '/matches',
+  TokenAuthorization,
+  validateTeam,
+  validateEqualTeams,
+  matchesInfos.create,
+);
 
 export default matchesRoutes;
